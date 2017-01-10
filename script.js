d93e794a8750d7bb944085213c4d20896382a90c
Onni <onni.heinonen98@gmail.comgit config --global user.name onni>
2017-01-10 15:43:35 +0200

    drag and drop tehtävän ensimmäinen versio

--- script.js
+++ script.js
@@ -163,7 +163,40 @@ function init(xml) {
         content.appendChild(leftElem);
         content.appendChild(rightElem);
       } else if(pageType === "dragdrop"){
-        content.innerHTML = $sivu.find("valiaikainen").text();
+        var $dragList = $sivu.find("drags");
+        var nextId = 0;
+        $dragList.find("drag").each(function(){
+          var $drag = $(this);
+          var dragElem = document.createElement("div");
+          var target = $drag.attr("target");
+          var dragTextElem = document.createElement("p");
+          dragTextElem.innerHTML = $drag.text();
+          dragElem.appendChild(dragTextElem);
+          dragElem.setAttribute("class", "drag");
+          dragElem.setAttribute("draggable", "true");
+          dragElem.setAttribute("ondragstart", "drag(event)");
+          dragElem.setAttribute("id", nextId);
+          dragElem.setAttribute("target", target);
+          content.appendChild(dragElem);
+          nextId = nextId + 1;
+        });
+        var dropWrapper = document.createElement("div");
+        content.appendChild(dropWrapper);
+        dropWrapper.setAttribute("class", "dropWrapper");
+        var $dropList = $sivu.find("drops");
+        $dropList.find("drop").each(function(){
+          var $drop = $(this);
+          var dropElem = document.createElement("div");
+          var target = $drop.attr("target");
+          var dropTextElem = document.createElement("p");
+          dropTextElem.innerHTML = $drop.text();
+          dropElem.appendChild(dropTextElem);
+          dropElem.setAttribute("class", "drop");
+          dropElem.setAttribute("ondrop", "drop(event)");
+          dropElem.setAttribute("ondragover", "allowDrop(event)");
+          dropElem.setAttribute("target", target);
+          dropWrapper.appendChild(dropElem);
+        });
       } else if(pageType === "kysely"){
         var leftElem = document.createElement("div");
         leftElem.setAttribute("class", "leftside");
@@ -317,56 +350,26 @@ function submitData() {
   });
 }
 
-//dragdrop testi
-$(function(){
-  var block1 = document.getElementById("block1");
-  $(block1).data("target", 4);
-  var block2 = document.getElementById("block2");
-  $(block2).data("target", 4);
-  var block3 = document.getElementById("block3");
-  $(block3).data("target", 4);
-  var block9 = document.getElementById("block9");
-  $(block9).data("target", 2);
-  var block4 = document.getElementById("block4");
-  $(block4).data("target", 1);
-  var block5 = document.getElementById("block5");
-  $(block5).data("target", 1);
-  var block6 = document.getElementById("block6");
-  $(block6).data("target", 2);
-  var block7 = document.getElementById("block7");
-  $(block7).data("target", 2);
-  var block10 = document.getElementById("block10");
-  $(block10).data("target", 3);
-  var block11 = document.getElementById("block11");
-  $(block11).data("target", 3);
-  var block12 = document.getElementById("block12");
-  $(block12).data("target", 3);
-  var block8 = document.getElementById("block8");
-  $(block8).data("target", 2);
+//drag and drop
 
-  var goal1 = document.getElementById("goal1");
-  $(goal1).data("id", 1);
-  var goal2 = document.getElementById("goal2");
-  $(goal2).data("id", 2);
-  var goal3 = document.getElementById("goal3");
-  $(goal3).data("id", 3);
-  var goal4 = document.getElementById("goal4");
-  $(goal4).data("id", 4);
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-  $( ".draggable" ).draggable();
-  $( ".droppable" ).droppable({
-    drop: handleDropEvent,
-  });
-});
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
 
-function handleDropEvent( event, ui ) {
-  var drop = $(this).data( 'id' );
-  var drag = ui.draggable.data( 'target' );
-  if(drop === drag){
-    ui.draggable.addClass( "droppedright" );
-    ui.draggable.removeClass( "droppedwrong" );
-  } else{
-    ui.draggable.addClass( "droppedwrong" );
-    ui.draggable.removeClass( "droppedright" );
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var dataElem = document.getElementById(data);
+  ev.target.appendChild(dataElem);
+  if(ev.target.getAttribute("target") === dataElem.getAttribute("target")){
+    $(dataElem).addClass("dragRight");
+    $(dataElem).removeClass("dragWrong");
+  }else{
+    $(dataElem).addClass("dragWrong");
+    $(dataElem).removeClass("dragRight");
   }
 }
